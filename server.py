2099a451227ac9ea63c11d93c0c3e96974aa2476
Horacio G. de Oro <hgdeoro@gmail.com>
2014-02-23 22:20:13 -0300

    Refactor storing of uuidCookie

--- server.py
+++ server.py
@@ -27,22 +27,35 @@ USER_ID = os.environ['SAMPLE_USERID']
 #
 UUID_COOKIE = os.environ.get('UUID_COOKIE', None)
 
+def store_uuid_cookie():
+    """
+    Generates an uuidCookie and store it in Radis.
+    Returns: uuid (string) if stored correctly
+    Returns: None if cookie couldn't be stored
+    """
+    if UUID_COOKIE is None:
+        uuid_cookie = str(uuid.uuid4())
+    else:
+        uuid_cookie = UUID_COOKIE
+
+    expire = 5  # 5 seconds
+    set_result = redis_server.set("cookie-" + uuid_cookie,
+                                  USER_ID,
+                                  expire,
+                                  nx=True)
+    if set_result is True:
+        return uuid_cookie
+    else:
+        return None
+
+
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def do_GET(self):
         # REDIS -> SET cookie-5ebc3e41-709c-4dc7-857c-15233c96516a 12345 EX 10 NX
-        if UUID_COOKIE is None:
-            uuid_cookie = str(uuid.uuid4())
-        else:
-            uuid_cookie = UUID_COOKIE
+        uuid_cookie = store_uuid_cookie()
 
-        expire = 5  # 5 seconds
-        set_result = redis_server.set("cookie-" + uuid_cookie,
-                                      USER_ID,
-                                      expire,
-                                      nx=True)
-
-        if set_result is True:
+        if uuid_cookie is not None:
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
